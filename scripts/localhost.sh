4ac66db23ff249e3719d25b7de642a83e573c5db
Arpit Tripathi <32494300+arpit04tripathi@users.noreply.github.com>
2026-05-08 12:09:39 +0100

    Rename (#3)
    * baseurl
    
    * local

--- scripts/localhost.sh
+++ scripts/localhost.sh
@@ -5,7 +5,7 @@ echo [arguments] $@
 echo =========================================================
 
 PORT=9999
-URL_PATH="jekyll-theme-leaf-example"
+URL_PATH="jekyll-theme-example-leaf"
 
 separator_start() {
   echo '================================================================================= v';
